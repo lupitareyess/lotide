a5cef0417fa600dbe9610ac3b304794195774610
Lupita Reyes <lupita.g.reyes@outlook.com>
2023-02-09 11:36:23 -0500

    Function that counts letters in string and outputs object with count

---
+++ countLetters.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed:${actual} === ${expected}`);
+  } if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed:${actual} !== ${expected}`);
+  }
+};
+
+// Function takes in sentence (as string) and returns a count of each of the letters in the sentence
+
+const countLetters = function(str) {
+  const lowerCaseStr = str.toLowerCase();
+  const result = {};
+  for (let letter of lowerCaseStr) {
+    if (!result[letter]) {
+      result[letter] = 1;
+    } else {
+      result[letter]++;
+    }
+  }
+  return result;
+
+};
+
+// TEST CODE
+const sentence = "hello"
+const result = countLetters(sentence);
+
+assertEqual(result.h, 1);
+assertEqual(result.e, 1);
+assertEqual(result.l, 2);
+assertEqual(result.o, 1);
