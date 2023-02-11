1f9a830076ea49b299965c8e7db960da8dca722f
Lupita Reyes <lupita.g.reyes@outlook.com>
2023-02-11 01:17:07 -0500

    Refactored to make happy path last thing retured

--- countOnly.js
+++ countOnly.js
@@ -10,19 +10,23 @@ const assertEqual = function(actual, expected) {
 // Will return an object containing counts of everything that the input object listed
 // Function will report back how many instances of each string were found in the allItems array of strings
 
+// continue skips the iteration
+
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
-  for (const item of allItems) {
-    if (itemsToCount[item]) {
-      if (results[item]) {
-        results[item] += 1;
-      } else {
-        results[item] = 1;
-      }
+  for (const key of allItems) {
+    if (!itemsToCount[key]) {
+      continue;
     }
+    if (!results[key]) {
+      results[key] = 0;
+    }
+
+    results[key]++;
   }
+
   return results;
-};
+}
 
 // TEST CODE
 const firstNames = [
