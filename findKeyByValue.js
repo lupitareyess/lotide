26206a71627699b66954dc2da427c3f97b2f5cb7
Lupita Reyes <lupita.g.reyes@outlook.com>
2023-02-10 23:24:04 -0500

    Function that returns once key matches given value

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed:${actual} !== ${expected}`);
+    return;
+  }
+  console.log(`✅✅✅ Assertion Passed:${actual} === ${expected}`);
+};
+
+
+// Takes in object and a value. Scans the object amd returns the first key which contains the given value
+// if no key found, return undefined 
+const findKeyByValue = function(obj, value) {
+  let keys = Object.keys(obj);
+  for (const key of keys) {
+    if (obj[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+}
+
+//TEST CASES
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
