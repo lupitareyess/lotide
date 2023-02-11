4fc019d469e2bfc07468bd3cfa4d5eb028ce2d3b
Lupita Reyes <lupita.g.reyes@outlook.com>
2023-02-11 01:17:45 -0500

    Remove else statements

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed:${actual} === ${expected}`);
-  } if (actual !== expected) {
+  if (actual !== expected) {
     console.log(`🛑🛑🛑 Assertion Failed:${actual} !== ${expected}`);
+    return;
   }
+  console.log(`✅✅✅ Assertion Passed:${actual} === ${expected}`);
 };
 
 // TEST CODE
