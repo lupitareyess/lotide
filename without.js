2917ae3c7d49b290fcc42839d6eeefe699db69ba
Lupita Reyes <lupita.g.reyes@outlook.com>
2023-02-11 01:06:59 -0500

    Implement function that removes unwanted elements

---
+++ without.js
@@ -0,0 +1,39 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } for (let item of arr1) {
+    if (arr1[item] !== arr2[item]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (!eqArrays(actual, expected)) {
+    console.log(`🛑🛑🛑 Assertion Failed:${actual} !== ${expected}`);
+  } else if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed:${actual} === ${expected}`);
+  }
+};
+
+
+// Create function that returns a subset of a given array, removing unwanted elements
+// Returns array with only those elements from the source, and not present in the itemsToRemove arrary
+// Source array should no be modified 
+
+const without = function(sourceArr, itemsToRemove) {
+  const results = [];
+  for (const item of sourceArr) {
+    if (!itemsToRemove.includes(item)) {
+      results.push(item);
+    }
+  }
+
+  return results;
+};
+
+// TEST CODE
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
+assertArraysEqual(without([1, 2, 3], [4]), [1, 2, 3]);
