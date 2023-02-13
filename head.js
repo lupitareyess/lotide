7d280fef7ea47291c649a649454e2f3bc7d8f483
Lupita Reyes <lupita.g.reyes@outlook.com>
2023-02-13 14:11:44 -0500

    Remove test code, export function

--- head.js
+++ head.js
@@ -1,21 +1,14 @@
-// ASSERTION FUNCTION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed:${actual} === ${expected}`);
-  } if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed:${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
-// HEAD FUNCTION
-// Return first element in an array - should not be returned as an array
+/**
+ * 
+ * @param {array} arr 
+ * @returns first element in array
+ */
 
 const head = function(arr) {
   return arr[0]
 };
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([6]), 6);
-assertEqual(head([]), undefined);
+module.exports = head;
 
